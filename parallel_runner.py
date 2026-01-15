4d143fd19337749d3b20dbb64a95dcfafea68c34
DavidGBUF <davidgalhego05@gmail.com>
2026-01-15 14:27:58 -0300

    Add new CSV result files for kuririnMaskablePPO experiments
    - Created new CSV files in results/results_resilient and results/results_sf directories.
    - The files contain data related to crash trials, recovery success, and latency metrics.
    - Each file includes a header row with relevant column names for analysis.

--- parallel_runner.py
+++ parallel_runner.py
@@ -24,7 +24,7 @@ if __name__ == '__main__':
     parser.add_argument('--n_sessions', type=int, help='(int) number of sessions', default=50)
     parser.add_argument('--n_players', type=int, help='(int) number of players', default=6)
     parser.add_argument('--threads', type=int, help='(int) number of cores to use', default=17)
-    parser.add_argument('--repetition', type=int, help='(int) repetitions', default=33)
+    parser.add_argument('--repetition', type=int, help='(int) repetitions', default=5)
     parser.add_argument('--sfc', type=str, help='(str) on or off', default='on')
     parser.add_argument('--alg', type=str, help='(str  algorithm name', default='kuririnMaskablePPO') # osfem, msf, musfico, ga;...
     parser.add_argument('--share', type=str, help='(str) whether to share sfs or not', default='y')
@@ -68,7 +68,7 @@ if __name__ == '__main__':
 
     # Parâmetros fixos (comuns a ambos os modos)
     avas = ['1.0']
-    number_of_fails = ['3']
+    number_of_fails = ['40']
 
     if RUN_MODE == 'single':
         # --- MODO SINGLE: Lógica original preservada ---
