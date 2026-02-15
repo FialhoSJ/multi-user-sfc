a401cda53e7c6487d91e858c0cedae2c7d98c7cd
DavidGBUF <davidgalhego05@gmail.com>
2026-02-15 13:02:04 -0300

    Refactor code structure for improved readability and maintainability

--- parallel_runner.py
+++ parallel_runner.py
@@ -40,11 +40,11 @@ if __name__ == '__main__':
     BATCH_FAIL_TARGETS = ['low_risk', 'med_risk', 'high_risk']
     
     # 3. Número de repetições por cenário
-    BATCH_TOTAL_RUNS = 4
+    BATCH_TOTAL_RUNS = 5
     
     # ATENÇÃO: Isso vai disparar 27 processos Python pesados simultaneamente.
     # Certifique-se de que sua máquina aguenta (CPU/RAM).
-    BATCH_PARALLEL_RUNS = 12
+    BATCH_PARALLEL_RUNS = 15
     
     # --- Configurações de Falha ---
     number_of_fails = ['20'] 
