6a01d4deada9b5a7f2a0d28f8e859906ad4d6520
DavidGBUF <davidgalhego05@gmail.com>
2026-02-27 13:53:39 -0300

    Preparando terreno dissertação erick

--- main.py
+++ main.py
@@ -58,9 +58,9 @@ def main():
     
     # 1. Configuração MACRO (Gera o Cronograma)
     parser.add_argument('--ava', type=str, default='0.97', help='Meta Global de Disponibilidade')
-    parser.add_argument('--number_of_fails', type=str, default='0', help='Number of node failures')
+    parser.add_argument('--number_of_fails', type=str, default='3', help='Number of node failures')
     parser.add_argument('--min_fail_duration', type=float, default=20, help='Min duration of node failure')
-    parser.add_argument('--crash_at', type=float, default=-1, help='Forcar falha em X segundos (Ex: [300, 400, 500]). Use -1 para aleatorio. Respeite o numero de falhas.')
+    parser.add_argument('--crash_at', type=float, default=[400,520,640], help='Forcar falha em X segundos (Ex: [300, 400, 500]). Use -1 para aleatorio. Respeite o numero de falhas.')
 
     # 2. Configuração MICRO (Define a Confiabilidade Base por Nível)
     # Valores entre 0.0 e 1.0 (Ex: 0.99 = 99% confiável)
